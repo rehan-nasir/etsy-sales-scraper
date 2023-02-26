3f63009b675385bbf1c659954fd548e1a06bd0f9
Rehan Nasir <111933400+rehan-nasir@users.noreply.github.com>
2023-02-26 18:00:06 -0500

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+import requests
+import concurrent.futures
+import re
+import time
+
+# Multithreading parsing for faster output, processes requests in parallel.
+
+
+def get_listing_info(url):
+    main_url = url
+    id_array = []
+    listing_url = "https://www.etsy.com/ca/listing/"
+    response = requests.get(main_url)
+    soup = BeautifulSoup(response.text, "lxml")
+    listings = soup.find_all("a")
+    for tag in listings:
+        if tag.has_attr("data-listing-id"):
+            id_array.append(tag["data-listing-id"])
+
+    def get_item_info(listing_id):
+        item = listing_url + listing_id
+        r = requests.get(item)
+        s = BeautifulSoup(r.text, "lxml")
+        num_of_sales = ""
+        seller = ""
+        regex = re.compile(r'\bsales\b')
+        span_tag = s.find('span', class_='wt-text-caption', text=regex)
+        if span_tag is not None:
+            num_of_sales = span_tag.getText().strip()
+        link_tag = s.find('link', attrs={'rel': 'alternate', 'type': 'application/rss+xml'})
+        if link_tag is not None:
+            title_attr = link_tag.get('title')
+            match = re.search(r'Shop RSS for (\w+) on Etsy', title_attr)
+            seller = match.group(1)
+        return "Seller: " + seller + "\n" + num_of_sales + "\n" + item + "\n----------------------------"
+
+    results = []
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for i in id_array:
+            results.append(executor.submit(get_item_info, i))
+
+    for result in results:
+        print(result.result())
+
+
+def main():
+    # The function below will print all info about all the sellers that appear on the home page. (Works on any Etsy URL)
+    # get_listing_info("https://www.etsy.com/ca/")
+
+    """
+    TO PARSE MULTIPLE URLS:
+    urls = ["https://www.etsy.com/ca/",
+    https://www.etsy.com/ca/c/jewelry-and-accessories?ref=catnav-10855,
+    https://www.etsy.com/ca/c/home-and-living?ref=catnav-891]
+
+    It is advised to add a delay between each function call to avoid overloading the server with requests.
+    Such as:
+    For url in urls:
+        get_listing_info(url)
+        time.sleep(10)
+
+    """
+
+
+if __name__ == '__main__':
+    main()
